129502c9efa993aed7b75e47390ec1c72b360b4b
Vikrant Sagar <sagar.vikrant@gmail.com>
2014-05-11 23:13:27 -0400

    commit 1

---
+++ scratch.lisp
@@ -0,0 +1,9 @@
+(defun sentence () (append (np) (vb)))
+(defun np () (append (Article) (Noun)))
+(defun vb () (append (Verb) (np)))
+(defun Article () (one-of '(the a)))
+(defun Noun () (one-of '(man ball woman table)))
+(defun Verb () (one-of '(hit took saw liked)))
+
+(defun one-of (set) (list (random-elt set)))
+(defun random-elt (choices) (elt choices (random (length choices))))
